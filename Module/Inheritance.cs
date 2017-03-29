40532e280e284f3ebd14b21d4b75d47c76462207
BaranovMykola <mapsg32@gmail.com>
2017-03-29 15:29:25 +0300

    Implemented first task

--- Module/Inheritance.cs
+++ Module/Inheritance.cs
@@ -11,16 +11,33 @@ namespace Module
         public static double Foo(double x, double e)
         {
             double sum = x * x / 4;
-            for (int k = 1; k < 2; k++)
+            //for (int k = 1; k < 2; k++)
+            //{
+            //    double modificator = (-1) * x * x / (4 * (k + 2) * (k + 2));
+            //    sum += sum * modificator;
+            //}
+            double deltaSum = 0;
+            int k = 1;
+            do
             {
                 double modificator = (-1) * x * x / (4 * (k + 2) * (k + 2));
-                sum += sum * modificator;
+                deltaSum = sum * modificator;
+                sum += deltaSum;
+                ++k;
             }
+            while (Math.Abs(deltaSum) > e);
             return sum;
+        }
+        public static void Tabulation(double a, double b, double h=1, double e = 0.001)
+        {
+            for (; a < b + h / 2; a += h)
+            {
+                Console.WriteLine("F({0}) = {1}", a, Foo(a, e));
+            }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(Foo(1,0.1));
+            Tabulation(0, 10, 0.9);
             Console.ReadKey();
         }
     }
