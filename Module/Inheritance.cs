78fd5e96ebc028f6f160b6ef63a89b3cb8f25fb8
BaranovMykola <mapsg32@gmail.com>
2017-03-29 15:22:00 +0300

    Implemented simple sum withoud accuraticy

--- Module/Inheritance.cs
+++ Module/Inheritance.cs
@@ -8,8 +8,20 @@ namespace Module
 {
     class Inheritance
     {
+        public static double Foo(double x, double e)
+        {
+            double sum = x * x / 4;
+            for (int k = 1; k < 2; k++)
+            {
+                double modificator = (-1) * x * x / (4 * (k + 2) * (k + 2));
+                sum += sum * modificator;
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Foo(1,0.1));
+            Console.ReadKey();
         }
     }
 }
