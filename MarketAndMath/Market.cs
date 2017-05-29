f657969279e67636e3596df10b2ea56a0038a1d8
BaranovMykola <mapsg32@gmail.com>
2017-05-29 14:11:38 +0300

    Implemented second task

---
+++ MarketAndMath/Market.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MarketAndMath
+{
+    [Serializable]
+    public class Market
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int Code { get; set; }
+        public string CustomerName { get; set; }
+
+        public Market()
+        {
+            
+        }
+
+        public Market(string name, int id, int price, int code, string customerName)
+        {
+            Name = name;
+            Id = id;
+            Price = price;
+            Code = code;
+            CustomerName = customerName;
+        }
+
+        public override string ToString() => $"Name [{Name}]\tId [{Id}]\tPrice [{Price}]\tCode[{Code}]\tCustomer name [{CustomerName}]";
+
+        public static List<Market> ReadFromFile(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+            List<Market> lst = new List<Market>();
+            foreach (var line in lines)
+            {
+                var elements = line.Split(' ');
+                lst.Add(new Market(elements[0], int.Parse(elements[1]), int.Parse(elements[2]), int.Parse(elements[3]), elements[4]));
+            }
+            return lst;
+        }
+    }
+}
---
+++ MarketAndMath/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MarketAndMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Market> lst = Market.ReadFromFile("../../Markets.txt");
+            lst = lst.OrderBy(s => s.Name).ToList();
+            foreach (var market in lst)
+            {
+                Console.WriteLine(market);
+            }
+            using (var stream = File.Open("../../Markets.xml", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Market>));
+                xml.Serialize(stream, lst);
+            }
+            var query =
+                from item in lst
+                group item by item.Id
+                into grp
+                let x = new {Id = grp.Key, Count = grp.Count(), Max = grp.Max(s => s.Price)}
+                orderby x.Count, x.Id
+                select x;
+            Console.WriteLine();
+            foreach (var item in query)
+            {
+                Console.WriteLine($"Quantity [{item.Count}]\tId [{item.Id}]\tMax price [{item.Max}]");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
